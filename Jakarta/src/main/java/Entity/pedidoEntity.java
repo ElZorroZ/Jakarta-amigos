1385d210b6b7669b9ef90f561a3ece508d58e560
Kraneowatt <gasparlopezjeifetz@gmail.com>
2025-09-23 16:12:57 -0300

    Otras cosas

--- Jakarta/src/main/java/Entity/pedidoEntity.java
+++ Jakarta/src/main/java/Entity/pedidoEntity.java
@@ -20,4 +20,44 @@ public class pedidoEntity {
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     private usuarioEntity idUsuario;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public productoEntity getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(productoEntity idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public int getPrecioTotal() {
+        return precioTotal;
+    }
+
+    public void setPrecioTotal(int precioTotal) {
+        this.precioTotal = precioTotal;
+    }
+
+    public usuarioEntity getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(usuarioEntity idUsuario) {
+        this.idUsuario = idUsuario;
+    }
 }
--- Jakarta/src/main/java/Entity/productoEntity.java
+++ Jakarta/src/main/java/Entity/productoEntity.java
@@ -21,5 +21,43 @@ public class productoEntity {
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     private categoriaEntity idCategoria;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public Long getTextoProducto() {
+        return textoProducto;
+    }
+
+    public void setTextoProducto(Long textoProducto) {
+        this.textoProducto = textoProducto;
+    }
+
+    public categoriaEntity getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(categoriaEntity idCategoria) {
+        this.idCategoria = idCategoria;
+    }
 }
