8ca84c54f10a29800f86913247ea95fcdcbc02e1
Kraneowatt <gasparlopezjeifetz@gmail.com>
2025-09-23 22:32:35 -0300

    mas

---
+++ Jakarta/src/main/java/Entity/productoInterface.java
@@ -0,0 +1,4 @@
+package Entity;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface productoInterface extends JpaRepository<productoEntity, Integer>{
+}
---
+++ Jakarta/src/main/java/Entity/usuarioInterface.java
@@ -0,0 +1,4 @@
+package Entity;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface usuarioInterface extends JpaRepository<usuarioEntity, Integer>{
+}
